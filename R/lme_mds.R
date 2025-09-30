07de7a2af7b8da9cad5adc0f28e76b3d7265ab4b
ismaRP <isma.kosspub@gmail.com>
2025-09-30 12:55:05 +0200

    Move lme util funcs to lme_mds

--- R/lme_mds.R
+++ R/lme_mds.R
@@ -214,6 +214,91 @@ predict_mds = function(model, estimates, new_q2e=NULL, logq=T){
 }
 
 
+## Predict sample function
+#' Title
+#'
+#' @param Sample
+#' @param Replicates
+#' @param Peptides
+#' @param Reliability
+#' @param resp Response variable, either q or Logq
+#'
+#' @param pars Contains the following parameters:
+#' \describe{
+#'     \item{data_I_s}{part of the data frame corresponding to Sample s.}
+#'     \item{alpha}{estimate for fixed effect}
+#'     \item{sigma2_S}{estimate for variance for random effect of sample.}
+#'     \item{sigma2_R}{estimate for variance for random effect of replication.}
+#'     \item{gamma}{estimate for half power on Reliability}
+#'     \item{sigma2}{estimate for variance for on the different peptides. This should be a named vector.}
+#'}
+#' @return tibble with predicted sample MDS and standard deviation
+#' @export
+#' @importFrom tibble tibble
+#' @examples
+predict_sample <- function(sample, replicate, pep_number, reliability, resp, pars) {
+
+  data_I_s = data.frame(
+    sample = sample, replicate = replicate, pep_number = pep_number,
+    reliability = reliability, resp = resp)
+
+  alpha = pars$alpha
+  sigma2_S = pars$sigma2_S
+  sigma2_R = pars$sigma2_R
+  gamma = pars$gamma
+  sigma2 = pars$sigma2
+
+  if (nrow(data_I_s) == 0) {
+    # prediction and variance without any data
+    E_X = 0
+    var_X = sigma2_S
+  } else {
+    # sanity check
+    if (length(unique(data_I_s$sample)) != 1) stop("Observations must come from the same sample.")
+    # build Xi
+    Xi = sigma2_S * matrix(1, nrow(data_I_s), nrow(data_I_s)) +
+      sigma2_R * outer(data_I_s$replicate, data_I_s$replicate, function(x,y){as.numeric(x == y)}) +
+      diag((data_I_s$reliability^(2*gamma)) * sigma2[as.character(data_I_s$pep_number)], nrow = nrow(data_I_s))
+    # prediction
+    # We don't need the estimate, as the ranef function provides it
+    # E_X = sigma2_S * sum(solve(Xi, data_I_s$resp - alpha[as.character(data_I_s$pep_number)]))
+    # variance
+    var_X = sigma2_S - (sigma2_S^2) * sum(solve(Xi, rep(1, nrow(data_I_s))))
+  }
+
+  # return
+  # names(E_X) = names(var_X) <- NULL
+  # return(tibble(MDS.PredictSample = E_X, sd = sqrt(var_X)))
+  return(tibble(sd = sqrt(var_X)))
+}
+
+
+#' Title
+#'
+#' @param m
+#' @param q_data
+#'
+#' @return
+#' @importFrom nlme fixef
+#' @importFrom stats coef
+#'
+#' @examples
+extract_estimates = function(m) {
+  q_data = m$data
+  alpha.m = fixef(m)
+  names(alpha.m) = substr(names(alpha.m), 9, 12) ## to align the code, remove Peptides from the names
+  tmp = coef(m$modelStruct$reStruct, FALSE) * (m$sigma^2)
+  sigma2_S.m = tmp["sample.var((Intercept))"] ## variance parameter estimate for sample
+  sigma2_R.m = tmp["replicate.var((Intercept))"] ## variance parameter estimate for replicate
+  gamma.m = coef(m$modelStruct$varStruct$A, FALSE)
+  sigma2.m = m$sigma * c("pep1" = 1, coef(m$modelStruct$varStruct$B, FALSE))^2
+  sigma2.m = sigma2.m[match(levels(q_data$pep_number), names(sigma2.m))]
+
+  return(list(alpha = alpha.m, sigma2_S = sigma2_S.m, sigma2_R = sigma2_R.m,
+              gamma = gamma.m, sigma2 = sigma2.m))
+}
+
+
 
 #' Quantile-Quantile plots of the linear mixed effect estimates per peptide
 #'
@@ -300,4 +385,6 @@ fvsr = function(mds_m, title="", peptides_user=NULL, label_idx=2,
     ggtitle(title)
   return(fvsr_plot)
 }
+
+
 
